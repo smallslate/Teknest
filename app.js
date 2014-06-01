60023de84f455677cdf937c3eda5b5a3b743800a
Sathya Vikram <support@smallslate.com>
2014-06-01 18:56:02 -0500

    Teknest

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Teknest server listening on port " + app.get('port'));
+});
